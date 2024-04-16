5ede57039cda6be235f8f3f6b0821d8a2a48454b
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-04-16 19:49:37 +0700

    responsive loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,7 @@ type Props = {}
 const loading = (props: Props) => {
   return (
     <div className='min-h-screen fixed z-[1000000] bg-accent w-full flex items-center justify-center top-0 left-0 right-0 bottom-0'>
-        <span className='flex items-center justify-center text-7xl text-primary'>
+        <span className='flex items-center justify-center lg:text-7xl text-4xl text-primary'>
             <MdOutlineDownloading className='animate-bounce'/>
             Loading
         </span>
