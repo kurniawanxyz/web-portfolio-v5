07c631b4ae88d65f846a92e2d186b837046e1cd1
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-04-16 19:46:53 +0700

    responsive

--- src/components/core/Navigation.tsx
+++ src/components/core/Navigation.tsx
@@ -31,9 +31,9 @@ const Navigation = (props: Props) => {
       
 
   return (
-<nav style={{top: `${(top < 0 ? top-10 : top)}px`, transition: "top 0.25s"}} className={`z-[1000] w-full fixed h-auto flex items-center justify-center py-3 px-0 lg:px-20`}>
+<nav style={{top: `${(top < 0 ? top-10 : top)}px`, transition: "top 0.25s"}} className={`z-[1000] w-full sticky h-auto flex items-center justify-center py-3 px-0 lg:px-20`}>
     <div className='flex justify-center group w-10/12 md:w-full'>
-        <ul className='flex gap-12 border-2 border-secondary px-3 py-2 rounded-full group-hover:ring-1 group-hover:ring-secondary'>
+        <ul className='flex lg:gap-12 gap-4 border-2 border-secondary px-3 py-2 rounded-full group-hover:ring-1 group-hover:ring-secondary'>
             {link.map((nav: Link, i: number) => (
                 <NavigationLink
                     key={i}
