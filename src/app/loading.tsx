21d4438f82c37516f0456ebc80db1d21751595a2
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-04-16 15:38:12 +0700

    loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import { MdOutlineDownloading } from "react-icons/md"
 type Props = {}
 
 const loading = (props: Props) => {
   return (
-    <div>loading...</div>
+    <div className='min-h-screen fixed z-[1000000] bg-accent w-full flex items-center justify-center top-0 left-0 right-0 bottom-0'>
+        <span className='flex items-center justify-center text-7xl text-primary'>
+            <MdOutlineDownloading className='animate-bounce'/>
+            Loading
+        </span>
+    </div>
   )
 }
 
